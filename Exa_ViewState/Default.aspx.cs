163d1520234019e188fd01fc26a019ffbbff55a0
Akash Maniar <akash@gmail.com>
2018-09-05 23:55:53 +0530

    little changes

--- Exa_ViewState/Default.aspx.cs
+++ Exa_ViewState/Default.aspx.cs
@@ -14,60 +14,8 @@ public partial class _Default : System.Web.UI.Page
     protected void cmdAdd_Click(object sender, EventArgs e)
     {
         //check if viewstate is set ot not
-        if (ViewState["no1"] != null)
-        {
-            String opr = ViewState["opr"].ToString();
 
-            int temp = int.Parse(ViewState["no1"].ToString());
-            int new_state = 0;
-            if (ansbox.Text != "")
-            {
-                new_state = int.Parse(ansbox.Text);
-            }
-            else
-            {
-                new_state = temp;
-            }
-
-            if (opr == "+")
-            {
-                //add
-                temp = temp + new_state;
-            }
-            else if (opr == "-")
-            {
-                //sub
-                temp = temp - new_state;
-            }
-            else if (opr == "*")
-            {
-                //mul
-                temp = temp * new_state;
-            }
-            else if (opr == "/")
-            {
-                //div
-                if (new_state != 0)
-                {
-                    temp = temp / new_state;
-                }
-                else
-                {
-                    //cannot divide by zero
-
-                }
-
-            }
-            ViewState["no1"] = temp;
-            outputbox.Text = temp.ToString();
-        }
-        else
-        {
-            //view state not saved
-
-            ViewState["no1"] = ansbox.Text;
-
-        }
+        calculate();
         //change opr to add
         ViewState["opr"] = "+";
         ansbox.Text = "";
@@ -76,58 +24,8 @@ public partial class _Default : System.Web.UI.Page
     protected void cmdSub_Click(object sender, EventArgs e)
     {
         //check if viewstate is set ot not
-        if (ViewState["no1"] != null)
-        {
-            String opr = ViewState["opr"].ToString();
 
-            int temp = int.Parse(ViewState["no1"].ToString());
-            int new_state = 0;
-            if (ansbox.Text != "") {
-                new_state = int.Parse(ansbox.Text);
-            }
-            else
-            {
-                new_state = temp;
-            }
-            if (opr == "+")
-            {
-                //add
-                temp = temp + new_state;
-            }
-            else if (opr == "-")
-            {
-                //sub
-                temp = temp - new_state;
-            }
-            else if (opr == "*")
-            {
-                //mul
-                temp = temp * new_state;
-            }
-            else if (opr == "/")
-            {
-                //div
-                if (new_state != 0)
-                {
-                    temp = temp / new_state;
-                }
-                else
-                {
-                    //cannot divide by zero
-
-                }
-
-            }
-            ViewState["no1"] = temp;
-            outputbox.Text = temp.ToString();
-        }
-        else
-        {
-            //view state not saved
-
-            ViewState["no1"] = ansbox.Text;
-
-        }
+        calculate();
         //change opr to add
         ViewState["opr"] = "-";
         ansbox.Text = "";
@@ -136,60 +34,8 @@ public partial class _Default : System.Web.UI.Page
     protected void cmdMul_Click(object sender, EventArgs e)
     {
         //check if viewstate is set ot not
-        if (ViewState["no1"] != null)
-        {
-            String opr = ViewState["opr"].ToString();
 
-            int temp = int.Parse(ViewState["no1"].ToString());
-            int new_state = 0;
-            if (ansbox.Text != "")
-            {
-                new_state = int.Parse(ansbox.Text);
-            }
-            else
-            {
-                new_state = temp;
-            }
-
-            if (opr == "+")
-            {
-                //add
-                temp = temp + new_state;
-            }
-            else if (opr == "-")
-            {
-                //sub
-                temp = temp - new_state;
-            }
-            else if (opr == "*")
-            {
-                //mul
-                temp = temp * new_state;
-            }
-            else if (opr == "/")
-            {
-                //div
-                if (new_state != 0)
-                {
-                    temp = temp / new_state;
-                }
-                else
-                {
-                    //cannot divide by zero
-
-                }
-
-            }
-            ViewState["no1"] = temp;
-            outputbox.Text = temp.ToString();
-        }
-        else
-        {
-            //view state not saved
-
-            ViewState["no1"] = ansbox.Text;
-
-        }
+        calculate();
         //change opr to add
         ViewState["opr"] = "*";
         ansbox.Text = "";
@@ -198,61 +44,8 @@ public partial class _Default : System.Web.UI.Page
     protected void cmdDiv_Click(object sender, EventArgs e)
     {
         //check if viewstate is set ot not
-        if (ViewState["no1"] != null)
-        {
-            String opr = ViewState["opr"].ToString();
 
-            int temp = int.Parse(ViewState["no1"].ToString());
-
-            int new_state = 0;
-            if (ansbox.Text != "")
-            {
-                new_state = int.Parse(ansbox.Text);
-            }
-            else
-            {
-                new_state = temp;
-            }
-
-            if (opr == "+")
-            {
-                //add
-                temp = temp + new_state;
-            }
-            else if (opr == "-")
-            {
-                //sub
-                temp = temp - new_state;
-            }
-            else if (opr == "*")
-            {
-                //mul
-                temp = temp * new_state;
-            }
-            else if (opr == "/")
-            {
-                //div
-                if (new_state != 0)
-                {
-                    temp = temp / new_state;
-                }
-                else
-                {
-                    //cannot divide by zero
-                    Response.Write("<h3 style='color:red'>Cannot Divide By Zero</style></h3>");
-                }
-
-            }
-            ViewState["no1"] = temp;
-            outputbox.Text = temp.ToString();
-        }
-        else
-        {
-            //view state not saved
-
-            ViewState["no1"] = ansbox.Text;
-
-        }
+        calculate();
         //change opr to add
         ViewState["opr"] = "/";
         ansbox.Text = "";
@@ -269,16 +62,46 @@ public partial class _Default : System.Web.UI.Page
         {
 
             //equal with operation
-            
+            calculate();
+          
+        }
+        ViewState["opr"] = "=";
+        ansbox.Text = "";
+        ansbox.Focus();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (ViewState["opr"] != null)
+        {
+            ViewState["opr"] = null;
+        }
+
+        if (ViewState["no1"] != null)
+        {
+            ViewState["no1"] = null;
+        }
+
+        ansbox.Text = "";
+        outputbox.Text = "";
+        Response.Redirect("Default.aspx");
+    }
+    protected void calculate()
+    {
+        if (ViewState["no1"] != null)
+        {
             String opr = ViewState["opr"].ToString();
-            
+
             int temp = int.Parse(ViewState["no1"].ToString());
             int new_state = 0;
-    
-            if(ansbox.Text != null && ansbox.Text != "")
+            if (ansbox.Text != "")
+            {
                 new_state = int.Parse(ansbox.Text);
+            }
             else
+            {
                 new_state = temp;
+            }
 
             if (opr == "+")
             {
@@ -311,27 +134,14 @@ public partial class _Default : System.Web.UI.Page
             }
             ViewState["no1"] = temp;
             outputbox.Text = temp.ToString();
-            
-
         }
-        ViewState["opr"] = "=";
-        ansbox.Text = "";
-        ansbox.Focus();
-    }
-
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        if (ViewState["opr"] != null) {
-            ViewState["opr"] = null;
-        }
-
-        if (ViewState["no1"] != null)
+        else
         {
-            ViewState["no1"] = null;
+            //view state not saved
+
+            ViewState["no1"] = ansbox.Text;
+
         }
 
-        ansbox.Text = "";
-        outputbox.Text = "";
-        Response.Redirect("Default.aspx");
     }
 }
